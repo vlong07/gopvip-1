084d901c4a86a8870946b7035dc5f8e455e36424
luvanlong2007 <vlongaccco@gmail.com>
2024-07-03 13:41:42 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,200 @@
+den = '[1;90m'
+luc = '[1;32m'
+trang = '[1;37m'
+red = '[1;31m'
+vang = '[1;33m'
+tim = '[1;35m'
+lamd = '[1;34m'
+lam = '[1;36m'
+purple = '\e[35m'
+hong = '[1;95m'
+thanh_xau = trang + '' + lam + '[' + vang + 'vL' + lam + '] ' + trang + ' ' + luc
+thanh_dep = trang + '' + lam + '[' + luc + 'vL' + lam + '] ' + trang + ' ' + luc
+import requests
+import json
+import os
+import sys
+from sys import platform
+from datetime import datetime
+from time import sleep, strftime
+import importlib
+
+#hihi 
+# main.py
+
+# my_script.py
+
+if __name__ == "__main__":
+    import sys
+    import os
+    
+    # Điều kiện kiểm tra tên file
+    expected_filename = "gopvip.py"  # Tên file bạn muốn kiểm tra
+    
+    # Lấy tên file hiện tại
+    current_filename = os.path.basename(sys.argv[0])
+    
+    # Kiểm tra nếu tên file không khớp
+    if current_filename != expected_filename:
+        print(f"Đổi Tên File Thành gopvip.py Sẽ Chạy Được")
+        sys.exit(1)
+    
+
+
+print("\033[1;39m   ╔═════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL Trao Đổi Sub \033[1;39m  ║ ")
+print("\033[1;39m   ╚═════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[1.1] \033[38;5;204mTOOL TDS TIKTOK + TIKTOK NOW")
+print("\033[38;5;155m      Nhập Số \033[1;36m[1.2] \033[38;5;204mTOOL AUTO TDS FACEBOOK ")
+print("\033[38;5;155m      Nhập Số \033[1;36m[1.3] \033[38;5;204mTOOL AUTO TDS PAGE PRO5 ")
+
+print("\033[1;39m   ╔═════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL Tương Tác Chéo\033[1;39m ║ ")
+print("\033[1;39m   ╚═════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[2.1] \033[38;5;204mTOOL TTC PAGE PRO5")
+print("\033[1;39m   ╔══════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL GOLIKE\033[1;39m  ║ ")
+print("\033[1;39m   ╚══════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[3.1] \033[38;5;204mTOOL GOLIKE TIKTOK")
+
+print("\033[1;39m   ╔═══════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL KIẾM XU\033[1;39m  ║ ")
+print("\033[1;39m   ╚═══════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[444] \033[38;5;204mTOOL RÚT XU - MUA XU \033[1;33m[VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[555] \033[38;5;204mTOOL HAMSTER TELEGRAM \033[1;33m[VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[666] \033[38;5;204mTOOL BUFF LIKE Machineliker.net \033[1;33m[By ĐHP VIP]")
+
+
+print("\033[1;39m   ╔═════════════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL LIÊN QUAN VỀ FACEBOOK\033[1;39m  ║ ")
+print("\033[1;39m   ╚═════════════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.1] \033[38;5;204mTOOL ADD BẠN BÈ")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.2] \033[38;5;204mTOOL NUÔI NICK FB LỎ")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.3] \033[38;5;204mTOOL CMT DẠO FB")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.4] \033[38;5;204mTOOL REG PAGE")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.5] \033[38;5;204mTOOL BUFF FOLLOW BẰNG PAGE PRO5")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.6] \033[38;5;204mTOOL CHUYỂN QUYỀN PAGE PRO5")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.7] \033[38;5;204mTOOL SPAM COMMENT FB \033[1;33m[NEW]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.8] \033[38;5;204mTOOL SPAM BOX MESSENGER \033[1;33m[NEW]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.9] \033[38;5;204mTOOL SPAM MESSENGER \033[1;33m[NEW]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.11] \033[38;5;204mTOOL SHARE ẢO MAX SPEED \033[1;33m[VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.12] \033[38;5;204mTOOL REG MAIL ẢO TẠO NICK FACEBOOK \033[1;33m[VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.13] \033[38;5;204mTOOL GET COOKIE BẰNG TK MK \033[1;33m[VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.14] \033[38;5;204mTOOL NUÔI NICK FB \033[1;33m[MỚI + VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.15] \033[38;5;204mTOOL GET TOKEN 16 LOẠI \033[1;33m[MỚI + VIP]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[5.16] \033[38;5;204mTOOL SPAM CMT PRO5 \033[1;33m[VIP]")
+
+print("\033[1;39m   ╔════════════════════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL SPAM SMS VÀ BUFF VIEW TIKTOK\033[1;39m  ║ ")
+print("\033[1;39m   ╚════════════════════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[6.1] \033[38;5;204mTOOL SPAM SMS")
+print("\033[38;5;155m      Nhập Số \033[1;36m[6.2] \033[38;5;204mTOOL SPAM SMS2")
+print("\033[38;5;155m      Nhập Số \033[1;36m[6.3] \033[38;5;204mTOOL BUFF VIEW TIKTOK \033[1;33m[CHỈ PC CHẠY ĐƯỢC]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[6.4] \033[38;5;204mTOOL BUFF VIEW TIKTOK PROXY")
+print("\033[1;39m   ╔═════════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL VỀ MAIL VÀ PROXY \033[1;39m  ║ ")
+print("\033[1;39m   ╚═════════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[7.1] \033[38;5;204mTOOL LỌC - CHECK MAIL [HMK]")
+print("\033[38;5;155m      Nhập Số \033[1;36m[7.2] \033[38;5;204mTOOL TÌM PROXY")
+print("\033[38;5;155m      Nhập Số \033[1;36m[7.3] \033[38;5;204mTOOL LỌC PROXY")
+
+print("\033[1;39m   ╔═══════════════════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL CÙI BẮP DỄ BỊ CRACK\033[1;39m  ║ ")
+print("\033[1;39m   ╚═══════════════════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[8.1] \033[38;5;204mTOOL CỦA HDTTOOL \033[1;33m[BẢN NEW] ")
+print("\033[38;5;155m      Nhập Số \033[1;36m[8.2] \033[38;5;204mTOOL CỦA C25 TOOL")
+
+print("\033[1;39m   ╔══════════════╗")
+print("\033[1;39m   ║ \033[1;34mTOOL DECODE\033[1;39m  ║ ")
+print("\033[1;39m   ╚══════════════╝")
+print("\033[38;5;155m      Nhập Số \033[1;36m[9.1] \033[38;5;204mTOOL DEC Kramer-Specter Deobf")
+print("\033[38;5;155m      Nhập Số \033[1;36m[9.2] \033[38;5;204mTOOL DEC Marshal/PYC")
+print("\033[38;5;155m      Nhập Số \033[1;36m[9.3] \033[38;5;204mTOOL DEC Marshal dump")
+
+chon = float(input('\033[1;31m     Nhập Số \033[1;32m: \033[1;33m'))
+
+import requests,os,sys, time
+def check_internet_connection():
+    try:
+        response = requests.get("https://google.com/", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+if not check_internet_connection():
+    print("\n\033[1;39mBạn bug 1 lần nữa sẽ bị dính botnet ngay nhé")
+    sys.exit(1) 
+if chon == 1.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/tdstiktok.txt').text)
+
+if chon == 1.2 :
+	exec(requests.get('https://raw.githubusercontent.com/luvanlong2007/gopvip/main/tds.py').text)
+if chon == 1.3 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/tdspro5.txt').text)
+if chon == 2.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/ttcpro5.txt').text)
+if chon == 3.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/goliketiktok.txt').text)
+if chon == 5.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/ketban.txt').text)
+if chon == 5.2 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/nuoinickfb.txt').text)
+if chon == 5.3 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/cmtdaofb.txt').text)
+if chon == 5.4 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/regpage.txt').text)
+if chon == 5.5 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/bufffl.txt').text)
+if chon == 5.6 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/chuyenquyenpro5.txt').text)
+if chon == 5.7 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/spamcmtfb.txt').text)
+if chon == 5.8 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/spambox.txt').text)
+if chon == 5.9 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/spammess.txt').text)
+if chon == 5.11 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/shareao.txt').text)
+if chon == 5.12 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/mailfb.py').text)
+if chon == 5.13 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/cookie.py').text)
+if chon == 5.14 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/nuoifb.py').text)
+if chon == 5.15 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/gettoken.py').text)
+if chon == 5.16 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/cmtpro5.py').text)
+if chon == 6.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/spamsms.txt').text)
+if chon == 6.2 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/spamsms2.txt').text)
+if chon == 6.3 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/viewtiktok.txt').text)
+if chon == 6.4 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/viewprx.txt').text)
+if chon == 7.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/mail.txt').text)
+if chon == 7.2 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/timproxy.txt').text)
+if chon == 7.3 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/locproxy.txt').text)
+if chon == 8.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/hdt.txt').text)
+if chon == 8.2 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/c25.txt').text)
+#tool kiếm xu
+if chon == 9.1 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/kramer.txt').text)
+if chon == 9.2 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/pyc.txt').text)
+if chon == 9.3 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/dump.txt').text)
+if chon == 444 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/kiemxu.txt').text)
+if chon == 555 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/ham.txt').text)
+if chon == 666 :
+	exec(requests.get('https://raw.githubusercontent.com/vlong07/vlong07/main/manlike.py').text)
+else :
+	print (" Sai Lựa Chọn ")
+	exit()
